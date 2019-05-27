a333b301b48de2fecb38399bda6163d38c139c91
HabeshaQueen <adhoadhigal.com>
2019-05-27 10:01:23 +0300

    add functionality to base.js

---
+++ static/js/base.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("Wards").style.marginLeft = "250px";
+}
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("Wards").style.marginLeft = "0";
+}
