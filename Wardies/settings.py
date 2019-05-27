90432ee9add3b06e9c471b17599d05084dcb2b5d
HabeshaQueen <adhoadhigal.com>
2019-05-27 11:12:14 +0300

    add regestration form

--- Wardies/settings.py
+++ Wardies/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = 'Wardies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awards',
+        'USER': 'adho',
+        'PASSWORD':'chukula13',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
